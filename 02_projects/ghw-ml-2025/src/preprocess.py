d8a97c32e2bf898bf687b63dcd3d288d69f06686
Mohammad Kamrul Hasan <kamrulhasan59246@gmail.com>
2026-01-07 13:26:25 +0000

    added preprocessor and stuff

---
+++ 02_projects/ghw-ml-2025/src/preprocess.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
+
+def preprocess_data(df: pd.DataFrame, target_cols: list[str]):
+  X = df.drop(columns=target_cols)
+
+  numeric_features = X.select_dtypes(include=['int64', 'float64']).columns.tolist()
+  categorical_features = X.select_dtypes(include=['object', 'category']).columns.tolist()
+
+  numeric_pipeline = SimpleImputer(strategy='median')
+  categorical_pipline = OneHotEncoder(handle_unknown='ignore')
+
+  preprocessor = ColumnTransformer(
+      transformers=[
+          ('num', StandardScaler(), num_cols),
+          ('cat', categorical_pipline, cat_cols)  
+      ]
+  )
+
+  return preprocessor, X;
