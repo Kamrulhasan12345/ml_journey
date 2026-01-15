66fdbc748a3b3cd81322762efe9f3f675dea2d1e
Mohammad Kamrul Hasan <kamrulhasan59246@gmail.com>
2026-01-15 15:25:39 +0000

    feat: added subrepo for hf-spaces

--- 02_projects/goemotions-api/api/main.py
+++ 02_projects/goemotions-api/api/main.py
@@ -1,24 +1,34 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from .classes import EmotionModelLoader, EmotionPredictor
+from api.classes import EmotionModelLoader, EmotionPredictor
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
-
-distilbert_loader = EmotionModelLoader(model_path="kamrulhasan12345/goemotions-distilbert")
-distilbert_predictor = EmotionPredictor(model_loader=distilbert_loader)
-
-bert_loader = EmotionModelLoader(model_path="kamrulhasan12345/goemotions-bert")
-bert_predictor = EmotionPredictor(model_loader=bert_loader)
 
 class TextInput(BaseModel):
     text: str
 
+@app.on_event("startup")
+async def load_model():
+    global distilbert_predictor
+    global bert_predictor
+    try:
+        logger.info("Loading models on startup...")
+        distilbert_model_loader = EmotionModelLoader(model_path="kamrulhasan12345/goemotions-distilbert")
+        distilbert_predictor = EmotionPredictor(distilbert_model_loader)
+        bert_model_loader = EmotionModelLoader(model_path="kamrulhasan12345/goemotions-bert")
+        bert_predictor = EmotionPredictor(bert_model_loader)
+        logger.info("Models loaded successfully.")
+    except Exception as e:
+        logger.error(f"Error loading models: {e}")
+
 @app.post("/predict-distilbert")
 def predict_distilbert(input: TextInput):
-    result = distilbert_predictor.predict(input.text)
-    return result
+    return distilbert_predictor.predict(input.text)
 
 @app.post("/predict-bert")
 def predict_bert(input: TextInput):
-    result = bert_predictor.predict(input.text)
-    return result
+    return bert_predictor.predict(input.text)
